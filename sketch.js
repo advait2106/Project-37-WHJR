e8fb6cf498ac7dc21ed5b6e7593e72df507dda79
advait2106 <76954638+advait2106@users.noreply.github.com>
2022-01-01 19:38:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+    question.isHidden;
+    backgroundColor ("blue")
+    gameState = over 
+    contestantInfo = mouseYIsPressed
+
+    if(allContestants   !==undefined){
+      fill("Blue");
+      textSize(20);
+      text("*NOTE: Contestant who answered correct are highlighted in green color!",130,230)   
+    for(var plr in allContestants){
+      var correctAns = "3"
+      if (correctAns === allContestants[plr].answer);
+      fill("Green")
+      
+      fill ("red"); 
+      
+    }
+  }
+  }
+}
